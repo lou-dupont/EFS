515e29436594e301985f24422a5cabde39a8f87d
lou-dupont <57115124+lou-dupont@users.noreply.github.com>
2020-03-03 20:54:23 +0100

    Première automatisation download+upload

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# EFS
-Lieux et horaires des collectes de don du sang
+# Lieux et horaires des collectes de don du sang (EFS)
+Le script récupère les données via le point d'entrée AJAX utilisé par la carte et le moteur de recherche présent sur le site de l'EFS : https://dondesang.efs.sante.fr/trouver-une-collecte
+
+Son principal intérêt est d'automatiser la sauvegarde des données téléchargées vers https://www.data.gouv.fr/fr/datasets/don-du-sang-lieux-et-horaires-des-collectes/ régulièrement.
+
+Pour cela, il faut une clef API de data.gouv.fr (qui est secrète). Le mode d'emploi est :
+
+* créer un fichier `params.py`au même niveau que le code `main.py`
+* dans ce fichier, saisir la clef au format `X_API_KEY = "ma_clef_api"`
+
+Configurer ensuite son serveur pour exécuter le script périodiquement.
---
+++ main.py
@@ -0,0 +1,39 @@
+import datetime
+import json
+import os
+from params import *
+import requests
+
+# Minimum valid size of 100 kb (to avoid overiding a previous valid file with a new empty one)
+MIN_VALID_SIZE = 100 * 1000
+
+url_source = "https://dondesang.efs.sante.fr/get-collects-ajax?neLon=180&neLat=90&swLon=-90&swLat=-180"
+url_api = "https://www.data.gouv.fr/api/1/datasets/5e5d89c28b4c410f100c3242/resources/205fae5e-e0b2-43f1-a198-2cd9dfd7a93d/"
+
+temp_json = 'current-collects.json'
+
+print("Downloading data")
+collects_json = requests.get(url_source).content
+with open(temp_json, 'wb') as temp_file:
+    temp_file.write(collects_json)
+
+if len(collects_json) > MIN_VALID_SIZE:
+
+    print("Uploading file")
+    headers = {
+        "X-API-KEY": X_API_KEY
+    }
+    response = requests.post(url_api + "upload/", files={'file': open(temp_json, 'rb')}, headers=headers)
+    os.remove(temp_json)
+    print(response.content)
+
+    print("Uploading metadata")
+    headers = {
+        "Content-Type": "application/json",
+        "X-API-KEY": X_API_KEY
+    }
+    data = { 
+        "published": datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+    }
+    response = requests.put(url_api, data=json.dumps(data), headers=headers)
+    print(response.content)
